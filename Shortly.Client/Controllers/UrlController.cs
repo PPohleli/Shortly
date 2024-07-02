2a24712e59e075d040e4f8c1c96a9ed786bc26cc
Phiwe-Pohleli <pohleliphiwe@gmail.com>
2024-07-02 19:21:38 +0200

    Cleaning up the Homepage controller and its Index view.

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shortly.Client.Data.Models;
 
 namespace Shortly.Client.Controllers
 {
@@ -6,7 +7,29 @@ namespace Shortly.Client.Controllers
     {
         public IActionResult Index()
         {
+            var tempData = TempData["SuccessMessage"];
+            var viewBag = ViewBag.Test1;
+            var viewdate = ViewData["Test 2"];
+
+            if (tempData != null)
+            {
+                ViewBag.SuccessMessage = TempData["SuccessMessage"].ToString();
+            }
+            else
+            {
+
+            }
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            var shortenedUrl = "'short";
+            TempData["SuccessMessage"] = "Successful!";
+            ViewBag.Test1 = "Test 1";
+            ViewData["Test 2"] = "Test 2";
+
+            return RedirectToAction("Index");
         }
     }
 }
