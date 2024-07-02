b565c9a6951c5f7aad03f1728a02123507338164
Phiwe-Pohleli <pohleliphiwe@gmail.com>
2024-07-02 21:31:19 +0200

    Designing the All links Page/View

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -7,27 +7,13 @@ namespace Shortly.Client.Controllers
     {
         public IActionResult Index()
         {
-            var tempData = TempData["SuccessMessage"];
-            var viewBag = ViewBag.Test1;
-            var viewdate = ViewData["Test 2"];
-
-            if (tempData != null)
-            {
-                ViewBag.SuccessMessage = TempData["SuccessMessage"].ToString();
-            }
-            else
-            {
-
-            }
+            
             return View();
         }
 
         public IActionResult Create()
         {
-            var shortenedUrl = "'short";
-            TempData["SuccessMessage"] = "Successful!";
-            ViewBag.Test1 = "Test 1";
-            ViewData["Test 2"] = "Test 2";
+            
 
             return RedirectToAction("Index");
         }
