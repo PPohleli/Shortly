776548402e4c878b7aa78fc5c231f62f8782b9ef
Phiwe-Pohleli <pohleliphiwe@gmail.com>
2024-07-02 21:47:24 +0200

    Passing dummy data from controller to All link Page/View.

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -7,8 +7,37 @@ namespace Shortly.Client.Controllers
     {
         public IActionResult Index()
         {
-            
-            return View();
+            //Fake Db data
+
+            var allUrls = new List<Url>()
+            {
+                new Url()
+                {
+                    Id = 1,
+                    OriginalLink = "https://original.com",
+                    ShortLink = "orgnl",
+                    NrOfClicks = 1,
+                    userId = 1,
+                },
+                new Url()
+                {
+                    Id = 2,
+                    OriginalLink = "https://link1.com",
+                    ShortLink = "lnk1",
+                    NrOfClicks = 2,
+                    userId = 2,
+                },
+                new Url()
+                {
+                    Id = 3,
+                    OriginalLink = "https://link2.com",
+                    ShortLink = "orgnl",
+                    NrOfClicks = 3,
+                    userId = 3,
+                }
+            };
+
+            return View(allUrls);
         }
 
         public IActionResult Create()
