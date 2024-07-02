17095b02fcd1d7b8bd7bdd8f1e0c6c425ac95151
Phiwe-Pohleli <pohleliphiwe@gmail.com>
2024-07-02 23:17:05 +0200

    Designing the login form

--- Shortly.Client/Controllers/AuthenticationController.cs
+++ Shortly.Client/Controllers/AuthenticationController.cs
@@ -8,5 +8,9 @@ namespace Shortly.Client.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
