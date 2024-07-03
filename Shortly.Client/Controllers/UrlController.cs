a1ef3239f61387aabbf7502cbfb8354aaf130313
Phiwe-Pohleli <pohleliphiwe@gmail.com>
2024-07-03 21:02:40 +0200

    Parsing arameters as Route Data.

--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -47,7 +47,12 @@ namespace Shortly.Client.Controllers
 
             return RedirectToAction("Index");
         }
-        public IActionResult Remove(int linkToRemove)
+        public IActionResult Remove(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Remove(int userId, int linkId)
         {
             return View();
         }
