fcd638e7872e1f15e98f8b86c2f5c4458e450379
Phiwe-Pohleli <pohleliphiwe@gmail.com>
2024-07-04 22:48:00 +0200

    Create a custom validation attribute to validate email address.

--- Shortly.Client/Data/ViewModels/RegisterVM.cs
+++ Shortly.Client/Data/ViewModels/RegisterVM.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Shortly.Client.Helpers.Validators;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shortly.Client.Data.ViewModels
 {
@@ -8,7 +9,7 @@ namespace Shortly.Client.Data.ViewModels
         public string FullName { get; set; }
 
         [Required(ErrorMessage = "Email address is required!")]
-        [RegularExpression(@"^\S+@\S+\.\S+$", ErrorMessage = "Invalid email address")]
+        [CustomEmailValidator(ErrorMessage = "Email address is not valid - custom")]
         public string EmailAddress { get; set; }
 
         [Required(ErrorMessage = "Password is required!")]
